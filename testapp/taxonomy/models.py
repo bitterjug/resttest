aab34922641165c028f7ee23ad0053e3b9bba2c4
Mark Skipper <marks@aptivate.org>
2014-01-25 20:15:31 +0000

    add simple model

--- testapp/taxonomy/models.py
+++ testapp/taxonomy/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Taxonomy(models.Model):
+    name = models.CharField(max_length=100)
+    # Other shit here about, e.g.
+    # Whether terms may overlap
+    # Whether there is hierarcy
+    # etc.
+
+
+class Term(models.Model):
+    taxonomy = models.ForeignKey(Taxonomy)
+    name = models.CharField(max_length=100)

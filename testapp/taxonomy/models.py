719aefa546798942ad4470cb4060dcd6954ca60b
Mark Skipper <marks@aptivate.org>
2014-01-25 20:33:52 +0000

    Add strings
    and a validity constraint

--- testapp/taxonomy/models.py
+++ testapp/taxonomy/models.py
@@ -1,14 +1,30 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Taxonomy(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(
+            max_length=100,
+            unique=True)
     # Other shit here about, e.g.
     # Whether terms may overlap
     # Whether there is hierarcy
     # etc.
 
+    def __str__(self):
+        """String represenation """
+        return self.name
 
+
+@python_2_unicode_compatible
 class Term(models.Model):
+    class Meta:
+        unique_together = ('name', 'taxonomy')
+
     taxonomy = models.ForeignKey(Taxonomy)
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        """String represenation """
+        return self.name

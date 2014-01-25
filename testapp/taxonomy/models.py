f0d149178cfbd1be3ca20eced48689ff2a1e458b
Mark Skipper <marks@aptivate.org>
2014-01-25 21:13:47 +0000

    Add related field name

--- testapp/taxonomy/models.py
+++ testapp/taxonomy/models.py
@@ -6,7 +6,8 @@ from django.utils.encoding import python_2_unicode_compatible
 class Taxonomy(models.Model):
     name = models.CharField(
             max_length=100,
-            unique=True)
+            unique=True
+    )
     # Other shit here about, e.g.
     # Whether terms may overlap
     # Whether there is hierarcy
@@ -22,7 +23,10 @@ class Term(models.Model):
     class Meta:
         unique_together = ('name', 'taxonomy')
 
-    taxonomy = models.ForeignKey(Taxonomy)
+    taxonomy = models.ForeignKey(
+            Taxonomy,
+            related_name='tags',
+    )
     name = models.CharField(max_length=100)
 
     def __str__(self):

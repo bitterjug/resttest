84efb7f26912bb35048f70d6a7790cf1f9939c93
Mark Skipper <marks@aptivate.org>
2014-01-27 23:09:45 +0000

    Get basic view working

--- testapp/testapp/static/testapp/js/main.js
+++ testapp/testapp/static/testapp/js/main.js
@@ -1,1 +1,20 @@
-alert("yup");
+(function($){
+
+
+var MyView = Backbone.View.extend({
+    el: $('#content'),
+
+    initialize: function(){
+        _.bindAll(this, 'render');
+        this.render();
+    },
+
+    render: function(){
+        $(this.el).html("<h2>From the script</h2>");
+    }
+});
+
+var myview = new MyView();
+
+
+})(jQuery);

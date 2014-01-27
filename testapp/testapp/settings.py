8cb3abc1f21d8d4e7f87ccc3707b834e7535fb50
Mark Skipper <marks@aptivate.org>
2014-01-27 22:41:27 +0000

    Add main page and static javascript main.js

--- testapp/testapp/settings.py
+++ testapp/testapp/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
@@ -23,6 +22,8 @@ SECRET_KEY = '92r!ushebaygzogvzr(02$52f=z13ioyoeg=ei)gvgc*d2jt0k'
 DEBUG = True
 
 TEMPLATE_DEBUG = True
+
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
 
 ALLOWED_HOSTS = []
 
@@ -38,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'rest_framework',
     'taxonomy',
+    'testapp',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -82,3 +84,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/mark/workspace/resttest/testapp/static'
--- testapp/testapp/urls.py
+++ testapp/testapp/urls.py
@@ -1,9 +1,10 @@
+from django.views.generic.base import TemplateView
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('taxonomy.urls')),
 )
